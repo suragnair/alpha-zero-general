3ec1256f145cd2688b18827213386864a11fd4fb
Shantanu Thakoor <shanu.thakoor@gmail.com>
2017-12-02 16:42:17 -0800

    Arena done

--- Arena.py
+++ Arena.py
@@ -1,8 +1,16 @@
 class Arena():
-    def __init__(self, player1, player2):
-    # player1 and player2 are functions which take in board, return action
-      pass
+    def __init__(self, player1, player2, game):
+    	# player1 and player2 are functions which take in board, return action
+    	self.player1 = player1
+    	self.player2 = player2
+    	self.game = game
 
     def playGame(self):
         # execute one game and return winner
-        pass
+        players = [self.player1, None, self.player2]
+        curPlayer = 1
+        board = game.getInitBoard()
+        while self.game.getGameEnded()!=0:
+        	action = players[curPlayer+1](self.game.getCanonicalForm(board, curPlayer))
+        	board, curPlayer = self.game.getNextState(board, curPlayer, action)
+        return self.game.getGameEnded(board, curPlayer)
