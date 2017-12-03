fixed rotation error, made some code more pythonic

Coach.py
@@ -27,22 +27,17 @@ class Coach():
             temp = int(episodeStep < self.args.tempThreshold)
 
             pi = self.mcts.getActionProb(canonicalBoard, temp=temp)
-            sym = self.game.getSymmetries(canonicalBoard)
-            for b in sym:
-                trainExamples.append([b, self.curPlayer, pi, None])
+            sym = self.game.getSymmetries(canonicalBoard, pi)
+            for b,p in sym:
+                trainExamples.append([b, self.curPlayer, p, None])
 
             action = np.argmax(pi)
             self.board, self.curPlayer = self.game.getNextState(self.board, self.curPlayer, action)
 
             r = self.game.getGameEnded(self.board, self.curPlayer)
-            if r!=0:
-                for i in range(len(trainExamples)):
-                    e = trainExamples[i]
-                    e[3] = r if self.curPlayer == e[1] else -r
-                    trainExamples[i] = (e[0],e[2],e[3])
-                break
 
-        return trainExamples
+            if r!=0:
+                return [(x[0],x[2],r*((-1)**(x[1]!=self.curPlayer))) for x in train_examples]
 
     def learn(self):
         # performs numIters x numEps games
@@ -72,4 +67,4 @@ class Coach():
             else:
                 print('NEW MODEL AWESOME')
                 self.nnet.save_checkpoint(folder=self.args.checkpoint, filename='best.pth.tar')
-                self.mcts = MCTS(self.game, self.nnet, self.args)
+                self.mcts = MCTS(self.game, self.nnet, self.args)   # reset search tree

OthelloGame.py
@@ -57,21 +57,26 @@ class OthelloGame():
         return -1
 
     def getCanonicalForm(self, board, player):
-        # return state if player==0, else return -state ?
+        # return state if player==1, else return -state if player==-1
         return player*board
 
-    def getSymmetries(self, board):
+    def getSymmetries(self, board, pi):
         # mirror, rotational
+        pi_board = np.reshape(pi, (self.n, self.n))
         l = []
+
         for i in range(1, 5):
             for j in [True, False]:
                 for k in [True, False]:
                     newB = np.rot90(board, i)
+                    newPi = np.rot90(pi_board, i)
                     if j:
                         newB = np.fliplr(newB)
+                        newPi = np.fliplr(newPi)
                     if k:
                         newB = np.flipud(newB)
-                    l += [newB]
+                        newPi = np.flipud(newPi)
+                    l += [newB, newPi.ravel()]
         return l
 
     def stringRepresentation(self, board):#, player):