8cd2982146286128563412730e6254810c72171f
suragnair <suragnair@hotmail.com>
2017-12-03 19:43:55 -0800

    reload from checkpoint

--- main.py
+++ main.py
@@ -8,16 +8,22 @@ args = dotdict({
     'numEps': 50,
     'tempThreshold': 15,
     'updateThreshold': 0.55,
-    'maxlenOfQueue': 25000,
+    'maxlenOfQueue': 50000,
     'numMCTSSims': 20,
     'arenaCompare': 20,
     'cpuct': 1,
 
-    'checkpoint': 'checkpoints',
+    'checkpoint': '/mnt/lol50x20/',
+    'load_model': True,
+    'load_folder_file': ('/mnt/lol50x20','checkpoint_4.pth.tar'),
 })
 
 if __name__=="__main__":
     g = OthelloGame(6)
     nnet = nn(g)
+
+    if args.load_model:
+        nnet.load_checkpoint(args.load_folder_file[0], args.load_folder_file[1])
+        
     c = Coach(g, nnet, args)
     c.learn()
