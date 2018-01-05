e8a1f4171fbf6c81b264acba63cbf3f24dcfa7e2
Surag Nair <suragnair@users.noreply.github.com>
2018-01-05 17:22:40 +0530

    Update NNet.py

--- othello/keras/NNet.py
+++ othello/keras/NNet.py
@@ -66,6 +66,6 @@ class NNetWrapper(NeuralNet):
     def load_checkpoint(self, folder='checkpoint', filename='checkpoint.pth.tar'):
         # https://github.com/pytorch/examples/blob/master/imagenet/main.py#L98
         filepath = os.path.join(folder, filename)
-        if not os.path.exists(filepath+'.meta'):
+        if not os.path.exists(filepath):
             raise("No model in path {}".format(filepath))
         self.nnet.model.load_weights(filepath)
