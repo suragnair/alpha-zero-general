ef5456d6b52b121bc9662cede8d416b272e98cf5
Evgeny <evg.tyurin@gmail.com>
2018-01-07 13:43:31 +0300

    Coach and Arena classes are compatible with games with draws.
    * Progress bar was added to Arena.playGames().

--- Arena.py
+++ Arena.py
@@ -1,4 +1,6 @@
 import numpy as np
+from pytorch_classification.utils import Bar, AverageMeter
+import time
 
 class Arena():
     """
@@ -26,7 +28,10 @@ class Arena():
         Executes one episode of a game.
 
         Returns:
-            winner: player who won the game (1 if player1, -1 if player2)
+            either
+                winner: player who won the game (1 if player1, -1 if player2)
+            or
+                draw result returned from the game that is neither 1, -1, nor 0.
         """
         players = [self.player2, None, self.player1]
         curPlayer = 1
@@ -48,7 +53,7 @@ class Arena():
             board, curPlayer = self.game.getNextState(board, curPlayer, action)
         if verbose:
             assert(self.display)
-            print("Turn ", str(it), "Player ", str(curPlayer))
+            print("Game over: Turn ", str(it), "Result ", str(self.game.getGameEnded(board, 1)))
             self.display(board)
         return self.game.getGameEnded(board, 1)
 
@@ -60,19 +65,52 @@ class Arena():
         Returns:
             oneWon: games won by player1
             twoWon: games won by player2
+            draws:  games won by nobody
         """
+        eps_time = AverageMeter()
+        bar = Bar('Arena.playGames', max=num)
+        end = time.time()
+        eps = 0
+        maxeps = int(num)
+
         num = int(num/2)
         oneWon = 0
         twoWon = 0
+        draws = 0
         for _ in range(num):
-            if self.playGame(verbose=verbose)==1:
+            gameResult = self.playGame(verbose=verbose)
+            if gameResult==1:
                 oneWon+=1
-            else:
+            elif gameResult==-1:
                 twoWon+=1
+            else:
+                draws+=1
+            # bookkeeping + plot progress
+            eps += 1
+            eps_time.update(time.time() - end)
+            end = time.time()
+            bar.suffix  = '({eps}/{maxeps}) Eps Time: {et:.3f}s | Total: {total:} | ETA: {eta:}'.format(eps=eps+1, maxeps=maxeps, et=eps_time.avg,
+                                                                                                       total=bar.elapsed_td, eta=bar.eta_td)
+            bar.next()
+
         self.player1, self.player2 = self.player2, self.player1
+        
         for _ in range(num):
-            if self.playGame(verbose=verbose)==-1:
-                oneWon+=1
-            else:
+            gameResult = self.playGame(verbose=verbose)
+            if gameResult==-1:
+                oneWon+=1                
+            elif gameResult==1:
                 twoWon+=1
-        return oneWon, twoWon
+            else:
+                draws+=1
+            # bookkeeping + plot progress
+            eps += 1
+            eps_time.update(time.time() - end)
+            end = time.time()
+            bar.suffix  = '({eps}/{maxeps}) Eps Time: {et:.3f}s | Total: {total:} | ETA: {eta:}'.format(eps=eps+1, maxeps=num, et=eps_time.avg,
+                                                                                                       total=bar.elapsed_td, eta=bar.eta_td)
+            bar.next()
+            
+        bar.finish()
+
+        return oneWon, twoWon, draws
