3b9da30485807345ac6ea0efac57bede111c2ed5
Evgeny Tyurin <evg.tyurin@gmail.com>
2018-02-14 10:40:22 +0300

    Detect cases when all valid moves were masked.
    * If a case detected then print message and do workaround.

--- MCTS.py
+++ MCTS.py
@@ -77,7 +77,17 @@ class MCTS():
             self.Ps[s], v = self.nnet.predict(canonicalBoard)
             valids = self.game.getValidMoves(canonicalBoard, 1)
             self.Ps[s] = self.Ps[s]*valids      # masking invalid moves
-            self.Ps[s] /= np.sum(self.Ps[s])    # renormalize
+            sum_Ps_s = np.sum(self.Ps[s])
+            if sum_Ps_s > 0:
+                self.Ps[s] /= sum_Ps_s    # renormalize
+            else:
+                # if all valid moves were masked make all valid moves equally probable
+                
+                # NB! All valid moves may be masked if either your NNet architecture is insufficient or you've get overfitting or something else.
+                # If you have got dozens or hundreds of these messages you should pay attention to your NNet and/or training process.   
+                print("All valid moves were masked, do workaround.")
+                self.Ps[s] = self.Ps[s] + valids
+                self.Ps[s] /= np.sum(self.Ps[s])
 
             self.Vs[s] = valids
             self.Ns[s] = 0
