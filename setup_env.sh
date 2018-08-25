6053c3e3108743c152c24da905482141a84d1728
Marco Lee <marco@goodnotesapp.com>
2018-08-25 18:21:03 +0800

    updated setup_env.sh to have default pytorch env.

--- setup_env.sh
+++ setup_env.sh
@@ -1,9 +1,9 @@
-DL_ENV=$1
+DL_ENV=${1:"pytorch"}
 NOTEBOOK_PORT=${2:-8888}
 VISDOM_PORT=${3:-8097}
 PYTORCH_IMAGE=pytorch:0.4.0-py3-gpu
 
-if [ "$DL_ENV"=="pytorch" ]; then
+if [ ${DL_ENV}=="pytorch" ]; then
     if [[ ! $(docker images -q ${pytorch:0.4.0-py3-gpu}) ]]; then
            docker build . -t ${PYTORCH_IMAGE} -f ./docker/Dockerfile.pytorch
     fi
