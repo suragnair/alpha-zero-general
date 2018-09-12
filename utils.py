9ce1048487b85e0e11498b741d4d9f948a76e57d
Giga Chang <gigayaya@gmail.com>
2018-09-13 02:25:37 +0800

    fix key error.

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
 class dotdict(dict):
     def __getattr__(self, name):
-        return self[name]
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError
