6a2a810c09b1cad5a7e2b430b18d0d3bcf57b6cf
Jernej Habjan <jernej.habjan96@gmail.com>
2018-11-03 20:29:50 +0100

    Model loaded on startup, saving session variables
    Session variables are saved for model, making prediction very fast. Also at the end of game, everything is cleared and destroyed.

--- get_action.py
+++ get_action.py
@@ -10,33 +10,50 @@ import unreal_engine as ue
 from TFPluginAPI import TFPluginAPI
 
 from MCTS import MCTS
-from td2020.TD2020Game import TD2020Game, display
+from td2020.TD2020Game import TD2020Game
 from td2020.keras.NNet import NNetWrapper as NNet
-from td2020.src.config import ACTS_REV, NUM_ACTS, visibility
+from td2020.src.config import ACTS_REV, NUM_ACTS
 from utils import dotdict
 
 """
 Get Action:
-This classes intended use is connecting to ue4 tensorflow plugin as client and execute predict on given board, that has been setted up as json board input
-
-# IMPORTANT - Dont use "display" which might want to import pygame
-
-Todo:
-    Garbage collection
-        gc.collect()
-        del self.n1p.model
+This classes intended use is connecting to ue4 TensorFlow plugin as client and execute predict on given board.
 """
 
 
+# noinspection PyPep8Naming
 class TD2020LearnAPI(TFPluginAPI):
     def __init__(self):
         self.owning_player = None
         self.initial_board_config = None
+        self.setup = False
+        self.g = None
+        self.graph_var = None
+        self.session_var = None
+        self.mcts = None
 
     def onSetup(self):
-        pass
+        # https://github.com/keras-team/keras/issues/2397#issuecomment-385317242
+
+        graph = tf.Graph()
+        with graph.as_default():
+            session = tf.Session()
+            with session.as_default():
+                current_directory = os.path.join(os.path.dirname(__file__), 'temp/')
+                self.g = TD2020Game(8)
+                n1 = NNet(self.g)
+                n1.load_checkpoint(current_directory, 'best.pth.tar')
+                args = dotdict({'numMCTSSims': 2, 'cpuct': 1.0})
+                self.mcts = MCTS(self.g, n1, args)
+
+                self.graph_var = graph
+                self.session_var = session
+
+                self.setup = True
 
     def onJsonInput(self, jsonInput):
+        if not self.setup:
+            return
         encoded_actors = jsonInput['data']
         initial_board_config = []
         for encoded_actor in encoded_actors:
@@ -55,38 +72,45 @@ class TD2020LearnAPI(TFPluginAPI):
 
         self.initial_board_config = initial_board_config
         self.owning_player = jsonInput['player']
-        with tf.Session() as sess:
-            current_directory = os.path.join(os.path.dirname(__file__), 'temp/')
-            g = TD2020Game(8)
-            n1 = NNet(g)
-            n1.load_checkpoint(current_directory, 'best.pth.tar')
-            args = dotdict({'numMCTSSims': 10, 'cpuct': 1.0})
-            mcts = MCTS(g, n1, args)
-            g.setInitBoard(self.initial_board_config)
-            b = g.getInitBoard()
-            n1p = lambda x: np.argmax(mcts.getActionProb(x, temp=0))
-            canonical_board = g.getCanonicalForm(b, self.owning_player)
+        ######
+        with self.graph_var.as_default():
+            with self.session_var.as_default():
+                self.g.setInitBoard(self.initial_board_config)
+                b = self.g.getInitBoard()
 
-            recommended_act = n1p(canonical_board)
-            y, x, action_index = np.unravel_index(recommended_act, [b.shape[0], b.shape[0], NUM_ACTS])
+                def n1p(board): return np.argmax(self.mcts.getActionProb(board, temp=0))
 
-            # gc.collect()
-            act = {"x": str(x), "y": str(y), "action": ACTS_REV[action_index]}
-            print("Printing recommended action >>>>>>>>>>>>>>>>>>>>>>>>" + str(act))
-            sess.close()
-        # gc.collect()
-        # self.recommended_act = act
+                canonical_board = self.g.getCanonicalForm(b, self.owning_player)
+
+                recommended_act = n1p(canonical_board)
+                y, x, action_index = np.unravel_index(recommended_act, [b.shape[0], b.shape[0], NUM_ACTS])
+
+                # gc.collect()
+                act = {"x": str(x), "y": str(y), "action": ACTS_REV[action_index]}
+                print("Printing recommended action >>>>>>>>>>>>>>>>>>>>>>>>" + str(act))
+
         return act
 
-
-
     def onBeginTraining(self):
-       pass
+        pass
 
     def run(self, args):
         pass
 
+    # noinspection PyUnusedLocal
+    def close(self, args):
+        print("Closing Get Action")
+        self.session_var.close()
+        self.owning_player = None
+        self.initial_board_config = None
+        self.setup = False
+        self.g = None
+        self.graph_var = None
+        self.session_var = None
+        self.mcts = None
+
 
 # required function to get our api
+# noinspection PyPep8Naming
 def getApi():
     return TD2020LearnAPI.getInstance()
