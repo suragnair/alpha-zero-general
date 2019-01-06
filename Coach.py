48d30188d4467daba938cb85e0ec6a33223dad8b
jl1990 <jlcarrasco1990@gmail.com>
2019-01-06 18:28:34 +0100

    Code refactoring

--- Coach.py
+++ Coach.py
@@ -148,7 +148,7 @@ class Coach():
         filename = os.path.join(folder, self.getCheckpointFile(iteration) + ".examples")
         with open(filename, "wb+") as f:
             Pickler(f).dump(self.trainExamplesHistory)
-        f.closed()
+        f.close()
 
     def loadTrainExamples(self):
         modelFile = os.path.join(self.args.load_folder_file[0], self.args.load_folder_file[1])
@@ -162,6 +162,6 @@ class Coach():
             print("File with trainExamples found. Read it.")
             with open(examplesFile, "rb") as f:
                 self.trainExamplesHistory = Unpickler(f).load()
-            f.closed()
+            f.close()
             # examples based on the model were already collected (loaded)
             self.skipFirstSelfPlay = True
--- pit.py
+++ pit.py
@@ -2,7 +2,7 @@ import Arena
 from MCTS import MCTS
 from othello.OthelloGame import OthelloGame, display
 from othello.OthelloPlayers import *
-from othello.pytorch.NNet import NNetWrapper as NNet
+from othello.keras.NNet import NNetWrapper as NNet
 from utils import *
 
 """
