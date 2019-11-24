2206e0ed0c97177da9aae8d4bbf99c88fe2d7ff9
Edward Tau <edward_tau@yahoo.com>
2019-11-23 19:28:20 -0800

    Bug fix and cleanup

--- pit.py
+++ pit.py
@@ -13,42 +13,41 @@ use this script to play any two agents against each other, or play manually with
 any agent.
 """
 
-mini_othello = False  # Play in 6x6 instead of the normal 8x8.
-human_vs_cpu = True
+mini_othello = True  # Play in 6x6 instead of the normal 8x8.
+opponent_type = "random"  # Possible opponent types: "random", "greedy", "human", "neural-net"
+
+
+def create_nn_player(mini):
+    # Create a trained neural network player
+    nn = NNet(g)
+    if mini:
+        nn.load_checkpoint('./pretrained_models/othello/pytorch/', '6x100x25_best.pth.tar')
+    else:
+        nn.load_checkpoint('./pretrained_models/othello/pytorch/', '8x8_100checkpoints_best.pth.tar')
+    args = dotdict({'numMCTSSims': 50, 'cpuct': 1.0})
+    mcts = MCTS(g, nn, args)
+    player = lambda x: np.argmax(mcts.getActionProb(x, temp=0))
+    return player
+
 
 if mini_othello:
     g = OthelloGame(6)
 else:
     g = OthelloGame(8)
 
-# all players
-rp = RandomPlayer(g).play
-gp = GreedyOthelloPlayer(g).play
-hp = HumanOthelloPlayer(g).play
+player1 = create_nn_player(mini_othello)
 
-
-
-# nnet players
-n1 = NNet(g)
-if mini_othello:
-    n1.load_checkpoint('./pretrained_models/othello/pytorch/','6x100x25_best.pth.tar')
+if opponent_type == "human":
+    player2 = HumanOthelloPlayer(g).play
+elif opponent_type == "greedy":
+    player2 = GreedyOthelloPlayer(g).play
+elif opponent_type == "random":
+    player2 = RandomPlayer(g).play
+elif opponent_type == "neural-net":
+    player2 = create_nn_player(mini_othello)
 else:
-    n1.load_checkpoint('./pretrained_models/othello/pytorch/','8x8_100checkpoints_best.pth.tar')
-args1 = dotdict({'numMCTSSims': 50, 'cpuct':1.0})
-mcts1 = MCTS(g, n1, args1)
-n1p = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
+    player2 = HumanOthelloPlayer(g).play
 
-if human_vs_cpu:
-    player2 = hp
-else:
-    n2 = NNet(g)
-    n1.load_checkpoint('./pretrained_models/othello/pytorch/', '8x8_100checkpoints_best.pth.tar')
-    args2 = dotdict({'numMCTSSims': 50, 'cpuct': 1.0})
-    mcts2 = MCTS(g, n2, args2)
-    n2p = lambda x: np.argmax(mcts2.getActionProb(x, temp=0))
-
-    player2 = n2p  # Player 2 is neural network if it's cpu vs cpu.
-
-arena = Arena.Arena(n1p, player2, g, display=OthelloGame.display)
+arena = Arena.Arena(player1, player2, g, display=OthelloGame.display)
 
 print(arena.playGames(2, verbose=True))
