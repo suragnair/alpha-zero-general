e5ec1f81b8fc8f8c19b08bb1032cadc35f6c9c41
Adam Lawson <adamjlawson22@gmail.com>
2020-02-05 17:09:08 +0000

    Add 3D TicTacToe

---
+++ tictactoe_3d/TicTacToePlayers.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+"""
+Random and Human-ineracting players for the game of TicTacToe.
+
+Author: Evgeny Tyurin, github.com/evg-tyurin
+Date: Jan 5, 2018.
+
+Based on the OthelloPlayers by Surag Nair.
+
+"""
+class RandomPlayer():
+    def __init__(self, game):
+        self.game = game
+
+    def play(self, board):
+        a = np.random.randint(self.game.getActionSize())
+        valids = self.game.getValidMoves(board, 1)
+        while valids[a]!=1:
+            a = np.random.randint(self.game.getActionSize())
+        return a
+
+
+class HumanTicTacToePlayer():
+    def __init__(self, game):
+        self.game = game
+
+    def play(self, board):
+        # display(board)
+        valid = self.game.getValidMoves(board, 1)
+        for i in range(len(valid)):
+            if valid[i]:
+                print(int(i/self.game.n), int(i%self.game.n))
+        while True: 
+            # Python 3.x
+            a = input()
+            # Python 2.x 
+            # a = raw_input()
+
+            x,y = [int(x) for x in a.split(' ')]
+            a = self.game.n * x + y if x!= -1 else self.game.n ** 2
+            if valid[a]:
+                break
+            else:
+                print('Invalid')
+
+        return a
