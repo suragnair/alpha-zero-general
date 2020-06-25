a24081af67d3d9148ed026ab47908739dc0d97c2
Carlos Sosa <c.sosa@pucp.edu.pe>
2020-06-25 15:18:34 -0500

    feat: add initial structure to game

---
+++ ultimate_tictactoe/UltimateTicTacToeGame.py
@@ -0,0 +1,34 @@
+from Game import Game
+
+
+class UltimateTicTacToeGame(Game):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def getInitBoard(self):
+        pass
+
+    def getBoardSize(self):
+        pass
+
+    def getActionSize(self):
+        pass
+
+    def getNextState(self, board, player, action):
+        pass
+
+    def getValidMoves(self, board, player):
+        pass
+
+    def getGameEnded(self, board, player):
+        pass
+
+    def getCanonicalForm(self, board, player):
+        pass
+
+    def getSymmetries(self, board, pi):
+        pass
+
+    def stringRepresentation(self, board):
+        pass
