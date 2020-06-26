0a1afd13d99328f4d6fcfb8bcd69849f76d1d637
Eduardo Cuya <eduardo.cuya@pucp.pe>
2020-06-26 16:29:29 -0500

    Cubi TicTacToe

--- main.py
+++ main.py
@@ -3,9 +3,42 @@ import logging
 import coloredlogs
 
 from Coach import Coach
-from othello.OthelloGame import OthelloGame as Game
-from othello.pytorch.NNet import NNetWrapper as nn
+from ultimate_tictactoe.UltimateTicTacToeGame import UltimateTicTacToeGame
+from ultimate_tictactoe.keras.NNet import NNetWrapper as nn
 from utils import *
+
+g = UltimateTicTacToeGame()
+
+# all players
+rp = RandomPlayer(g).play
+gp = GreedyOthelloPlayer(g).play
+hp = HumanOthelloPlayer(g).play
+
+
+# nnet players
+n1 = NNet(g)
+if mini_othello:
+    n1.load_checkpoint('./pretrained_models/othello/pytorch/','6x100x25_best.pth.tar')
+else:
+    n1.load_checkpoint('./pretrained_models/othello/pytorch/','8x8_100checkpoints_best.pth.tar')
+args1 = dotdict({'numMCTSSims': 50, 'cpuct':1.0})
+mcts1 = MCTS(g, n1, args1)
+n1p = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
+
+if human_vs_cpu:
+    player2 = hp
+else:
+    n2 = NNet(g)
+    n2.load_checkpoint('./pretrained_models/othello/pytorch/', '8x8_100checkpoints_best.pth.tar')
+    args2 = dotdict({'numMCTSSims': 50, 'cpuct': 1.0})
+    mcts2 = MCTS(g, n2, args2)
+    n2p = lambda x: np.argmax(mcts2.getActionProb(x, temp=0))
+
+    player2 = n2p  # Player 2 is neural network if it's cpu vs cpu.
+
+arena = Arena.Arena(n1p, player2, g, display=OthelloGame.display)
+
+print(arena.playGames(2, verbose=True))
 
 log = logging.getLogger(__name__)
 
