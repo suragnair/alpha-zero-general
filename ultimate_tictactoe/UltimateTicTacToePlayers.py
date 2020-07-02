f443f9ed3ea31205638017f6b7a9417d66740898
Carlos Sosa <c.sosa@pucp.edu.pe>
2020-07-01 21:08:23 -0500

    delete test

--- ultimate_tictactoe/UltimateTicTacToePlayers.py
+++ ultimate_tictactoe/UltimateTicTacToePlayers.py
@@ -41,31 +41,3 @@ class HumanUltimateTicTacToePlayer():
                 print('Invalid')
 
         return a
-
-
-class RLUTTTPlayer:
-    def __init__(self, learningModel):
-        self.learningAlgo = learningModel
-        super(RLUTTTPlayer, self).__init__()
-
-    def makeNextMove(self, board):
-        if not (board.is_win(1) or board.is_win(-1)):
-            if random.uniform(0, 1) < 0.8:
-                choices = {}
-                legal_space = board.get_legal_moves()
-                for lx, ly in legal_space:
-                    area = board.get_area(lx, ly)
-                    possible = Board(board.n)
-                    possible.copy(board)
-                    possible.execute_move((lx, ly), 1)
-                    choices[(lx, ly)] = self.learningAlgo.getBoardStateValue(self.player, board,
-                                                                             possible.pieces.reshape(81, ))
-                pickOne = max(choices, key=choices.get)
-            else:
-                legal = board.get_legal_moves()
-                pickOne = random.choice(legal)
-
-            return board.N * pickOne[0] + pickOne[1]
-
-    def loadLearning(self, filename):
-        self.learningAlgo.loadLearning(filename)
