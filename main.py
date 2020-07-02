f8f79ec701db6c2a07c81e0a0118546c7ae8b62f
Carlos Sosa <c.sosa@pucp.edu.pe>
2020-07-01 22:47:22 -0500

    revert to initial state

--- main.py
+++ main.py
@@ -1,51 +1,57 @@
-import os
+import logging
 
-os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
+import coloredlogs
 
 from Coach import Coach
-from ultimate_tictactoe.UltimateTicTacToeGame import UltimateTicTacToeGame
-from ultimate_tictactoe.keras.NNet import NNetWrapper as nn
+from othello.OthelloGame import OthelloGame as Game
+from othello.pytorch.NNet import NNetWrapper as nn
 from utils import *
 
-print('Loading %s...', UltimateTicTacToeGame.__name__)
+log = logging.getLogger(__name__)
 
-g = UltimateTicTacToeGame()
-
-print('Loading %s...', nn.__name__)
-nnet = nn(g)
-#
-
-#  log.info('Loading the Coach...')
+coloredlogs.install(level='INFO')  # Change this to DEBUG to see more info.
 
 args = dotdict({
-    'numIters': 15,
-    'numEps': 100,  # Number of complete self-play games to simulate during a new iteration.
-    'tempThreshold': 15,
-    'updateThreshold': 0.6,  # During arena playoff, new neural net will be accepted if threshold or more of games
-    # are won.
-    'maxlenOfQueue': 2000,  # Number of game examples to train the neural networks.
-    'numMCTSSims': 25,  # Number of games moves for MCTS to simulate.
-    'arenaCompare': 10,  # Number of games to play during arena play to determine if new net will be accepted.
+    'numIters': 1000,
+    'numEps': 100,              # Number of complete self-play games to simulate during a new iteration.
+    'tempThreshold': 15,        #
+    'updateThreshold': 0.6,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
+    'maxlenOfQueue': 200000,    # Number of game examples to train the neural networks.
+    'numMCTSSims': 25,          # Number of games moves for MCTS to simulate.
+    'arenaCompare': 40,         # Number of games to play during arena play to determine if new net will be accepted.
     'cpuct': 1,
 
-    'checkpoint': './temp_2/',
+    'checkpoint': './temp/',
     'load_model': False,
-    'load_folder_file': ('./temp/', 'checkpoint_5.h5'),
+    'load_folder_file': ('/dev/models/8x100x50','best.pth.tar'),
     'numItersForTrainExamplesHistory': 20,
 
 })
 
-if args.load_model:
-    print('Loading checkpoint "%s/%s"...', args.load_folder_file)
-    nnet.load_checkpoint(args.load_folder_file[0], args.load_folder_file[1])
-else:
-    print('Not loading a checkpoint!')
 
-c = Coach(g, nnet, args)
+def main():
+    log.info('Loading %s...', Game.__name__)
+    g = Game(6)
 
-if args.load_model:
-    print("Loading 'trainExamples' from file...")
-    c.loadTrainExamples()
+    log.info('Loading %s...', nn.__name__)
+    nnet = nn(g)
 
-print('Starting the learning process 🎉')
-c.learn()
+    if args.load_model:
+        log.info('Loading checkpoint "%s/%s"...', args.load_folder_file)
+        nnet.load_checkpoint(args.load_folder_file[0], args.load_folder_file[1])
+    else:
+        log.warning('Not loading a checkpoint!')
+
+    log.info('Loading the Coach...')
+    c = Coach(g, nnet, args)
+
+    if args.load_model:
+        log.info("Loading 'trainExamples' from file...")
+        c.loadTrainExamples()
+
+    log.info('Starting the learning process 🎉')
+    c.learn()
+
+
+if __name__ == "__main__":
+    main()
