6c2c0e1365305c997a6901c6a774237ad1b8bdf9
Carlos Sosa <c.sosa@pucp.edu.pe>
2020-07-02 01:34:01 -0500

    revert change on wrong file

--- othello/OthelloPlayers.py
+++ othello/OthelloPlayers.py
@@ -22,16 +22,16 @@ class HumanOthelloPlayer():
         valid = self.game.getValidMoves(board, 1)
         for i in range(len(valid)):
             if valid[i]:
-                print("[", int(i/self.game.N), int(i%self.game.N), end="] ")
+                print("[", int(i/self.game.n), int(i%self.game.n), end="] ")
         while True:
             input_move = input()
             input_a = input_move.split(" ")
             if len(input_a) == 2:
                 try:
                     x,y = [int(i) for i in input_a]
-                    if ((0 <= x) and (x < self.game.N) and (0 <= y) and (y < self.game.N)) or \
-                            ((x == self.game.N) and (y == 0)):
-                        a = self.game.N * x + y if x != -1 else self.game.N ** 2
+                    if ((0 <= x) and (x < self.game.n) and (0 <= y) and (y < self.game.n)) or \
+                            ((x == self.game.n) and (y == 0)):
+                        a = self.game.n * x + y if x != -1 else self.game.n ** 2
                         if valid[a]:
                             break
                 except ValueError:
