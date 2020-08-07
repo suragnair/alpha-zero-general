17a0b5b6175670d7c1bd23669e64a573a4262bb7
Mikhail Simin <mikhail.simin@gmail.com>
2020-08-06 20:37:57 -0700

    Change MCTS to use game Canonicalization
    MCTS assumes that board difference between P1 and P2 is strictly negativity. In case the data is more complex than that we can rely on already-required getCanonicalForm() method of the Game.

--- MCTS.py
+++ MCTS.py
@@ -99,7 +99,7 @@ class MCTS():
 
             self.Vs[s] = valids
             self.Ns[s] = 0
-            return -v
+            return self.game.getCanonicalForm(canonicalBoard, -1)
 
         valids = self.Vs[s]
         cur_best = -float('inf')
@@ -133,4 +133,4 @@ class MCTS():
             self.Nsa[(s, a)] = 1
 
         self.Ns[s] += 1
-        return -v
+        return self.game.getCanonicalForm(canonicalBoard, -1)
