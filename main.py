018f65ee1ef56b87c8a9049353d4130946d03a9a
pavolkacej <75344937+pavolkacej@users.noreply.github.com>
2021-12-05 17:13:50 +0100

    updated arguments (#261)
    Co-authored-by: Pavol Kacej <pavol.kacej@student.tuke.sk>

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def main():
     nnet = nn(g)
 
     if args.load_model:
-        log.info('Loading checkpoint "%s/%s"...', args.load_folder_file)
+        log.info('Loading checkpoint "%s/%s"...', args.load_folder_file[0], args.load_folder_file[1])
         nnet.load_checkpoint(args.load_folder_file[0], args.load_folder_file[1])
     else:
         log.warning('Not loading a checkpoint!')
