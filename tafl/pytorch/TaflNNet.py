b1b76463b1cb24054d9634be5ea2b68ed5ff00f6
Surag Nair <10693069+suragnair@users.noreply.github.com>
2022-06-19 14:06:09 -0700

    removed unused lines pytorch

--- tafl/pytorch/TaflNNet.py
+++ tafl/pytorch/TaflNNet.py
@@ -7,8 +7,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torchvision import datasets, transforms
-from torch.autograd import Variable
 
 class TaflNNet(nn.Module):
     def __init__(self, game, args):
